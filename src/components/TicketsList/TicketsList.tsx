fef5d10f69a2741284e4e01847db97820c41445f
takkeR-DEV <takkerwin@yandex.ru>
2023-02-09 11:49:36 +0300

    edits_keys

--- src/components/TicketsList/TicketsList.tsx
+++ src/components/TicketsList/TicketsList.tsx
@@ -2,17 +2,18 @@ import React, { FC } from 'react';
 import Tickets from '../Tickets/Tickets';
 import { FilteredDataType } from '../../types/tickets';
 
-function counter() {
-  let maxId = 1;
-  return () => maxId++;
-}
-
-const maxId = counter();
 const TicketsList: FC<FilteredDataType> = ({ filterTicketsData }) => {
   return (
     <div>
       {filterTicketsData.map((data) => {
-        return <Tickets key={maxId()} price={data.price} carrier={data.carrier} segments={data.segments} />;
+        return (
+          <Tickets
+            key={data.price + data.segments[0].date + data.segments[0].duration + data.segments[1].date}
+            price={data.price}
+            carrier={data.carrier}
+            segments={data.segments}
+          />
+        );
       })}
     </div>
   );
